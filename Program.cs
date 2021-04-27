0983b4a9f5c41ac52f5b6172ade0428a5f678888
Arthur Ringoringo <42938790+arthurringoringo@users.noreply.github.com>
2021-04-27 10:49:01 +0700

    UploadSolution

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AppManString2IntTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any string");
+
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                //Check weather it its an number with coresponsiding ASCII index
+                if (input[i] >= 48 && input[i] <= 57)
+                {
+                    int number = input[i];
+                    // Convert to string with ASCII offset
+                    number = number - 48;
+                    Console.Write(number);
+
+                    
+                }
+            }
+        }
+    }
+}
